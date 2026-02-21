604190eb0dbc4901b237618db7385423fa0c7b71
Sagar2401 <prajaptisagar81410@gmail.com>
2026-02-21 17:49:11 +0530

    Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={jetbrainsMono.className}>{children}</body>
-      <GoogleTagManager gtmId={process.env.NEXT_GTM_ID as string} />
+      <GoogleTagManager gtmId={"GTM-W7232QRH"} />
     </html>
   );
 }
